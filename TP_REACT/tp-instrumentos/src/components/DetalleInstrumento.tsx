73a4f7559e227271a4da53b6adfd8a9e2e8ae877
Emilia <emiartola@gmail.com>
2022-04-22 23:02:48 -0300

    TP_REACT

--- TP_REACT/tp-instrumentos/src/components/DetalleInstrumento.tsx
+++ TP_REACT/tp-instrumentos/src/components/DetalleInstrumento.tsx
@@ -32,71 +32,47 @@ export const DetalleInstrumento = () => {
         }
         getInstrumentoResto();
     }, []);
-    
+
+    // 
+
     return (
-                <>
-                <Navigation></Navigation>
-                <Container>
-                        <Row> 
-                        <Col><br/><img alt="instrumento" className="minAltoImg" src={"http://localhost:3000/images/"+instrumento?.imagen}  /></Col>
-                            <Col>
-                                <Row>
-                                    <Col><h1>{instrumento?.marca}</h1></Col>
-                                </Row>
-                                <Row>
-                                <Col>Precio: <h2>${instrumento?.precio}</h2></Col>
-                                </Row>
-                                <Row>
-                                    <Col>Descripción: <h2>{instrumento?.descripcion}</h2></Col>
-                                </Row>
-        
-                            </Col>
-                        </Row>
-        
-                        <Row>
-                            <Col><Nav.Link href="/"><h3>Volver</h3></Nav.Link></Col>
-                        </Row>                
-                        </Container>
-                </>
-            )
-        }
-//     return (
-//         <>
-//             <div className="container">
-//                 <div className="row">
-//                     <div className="col-lg-8">
-//                         <img
-//                             style={{ maxWidth: "400px", maxHeight: "400px" }}
-//                             src={`images/${instrumento?.imagen}`}
-//                             alt="instData"
-//                             className="card-img"
-//                         />
-//                         <p>Descripción :</p>
-//                         <p>{instrumento?.descripcion}</p>
-//                     </div>
-//                     <div className="col-lg-4 mt-3">
-//                         <p>{instrumento?.cantidadVendida} Vendidos</p>
-//                         <h2>{instrumento?.instrumento}</h2>
-//                         <h2 className="mt-5">${instrumento?.precio}</h2>
-//                         <h5>Marca : {instrumento?.marca}</h5>
-//                         <h5>Modelo : {instrumento?.modelo}</h5>
-//                         {instrumento?.costoEnvio === "G" ? (
-//                             <h5 style={{ color: "green" }}>
-//                                 {" "}
-//                                 <img src={`../assets/img/camion.png`} alt="envioIcon" />
-//                                 Envio Gratis A Todo El Pais
-//                             </h5>
-//                         ) : (
-//                             <h5 style={{ color: "orange" }}>
-//                                 Costo Envio Interior De Argentina ${instrumento?.precio}
-//                             </h5>
-//                         )}
-//                         <button className="btn btn-outline-primary mt-5">
-//                             Agregar Al Carrito
-//                         </button>
-//                     </div>
-//                 </div>
-//             </div>
-//         </>
-//     );
-// };
+        <>
+        <Navigation></Navigation>
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-8">
+                        <img
+                            style={{ maxWidth: "400px", maxHeight: "400px" }}
+                            src={"http://localhost:3000/images/" + instrumento?.imagen}
+                            alt="instrumento"
+                            className="card-img"
+                        />
+                        <p>Descripción :</p>
+                        <p>{instrumento?.descripcion}</p>
+                    </div>
+                    <div className="col-lg-4 mt-3">
+                        <p>{instrumento?.cantidadVendida} Vendidos</p>
+                        <h2>{instrumento?.instrumento}</h2>
+                        <h2 className="mt-5">${instrumento?.precio}</h2>
+                        <h5>Marca : {instrumento?.marca}</h5>
+                        <h5>Modelo : {instrumento?.modelo}</h5>
+
+                        Costo Envío:
+                        {instrumento?.costoEnvio === "G" ? (
+                            <h5 style={{ color: "green" }}>
+                                Envío gratis
+                            </h5>
+                        ) : (
+                            <h5 style={{ color: "orange" }}>
+                                 ${instrumento?.costoEnvio}
+                            </h5>
+                        )}
+                        <button className="btn btn-outline-primary mt-5">
+                            Agregar Al Carrito
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
